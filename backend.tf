9b93d560123e74b5c89cb90ceaf4319c8db8d200
vamshivynala <vamshivynala11@gmail.com>
2024-03-08 15:21:58 +0530

    Good Commit

---
+++ backend.tf
@@ -0,0 +1,8 @@
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "demo-resources"
+#     storage_account_name = "mystreaccount"
+#     container_name       = "submanagercontainer"
+#     key                  = "submanagercontainer.terraform.tfstate"
+#   }
+# }
